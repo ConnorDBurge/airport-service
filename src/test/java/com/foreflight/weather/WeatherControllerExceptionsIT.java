f2e7a9cfbf6ec47f390d77e6cb246daa23b7f3d3
ConnorDBurge <connor.d.burge@gmail.com>
2023-09-16 11:58:39 -0400

    removed external API mocks!

--- src/test/java/com/foreflight/weather/WeatherControllerExceptionsIT.java
+++ src/test/java/com/foreflight/weather/WeatherControllerExceptionsIT.java
@@ -1,32 +1,23 @@
 package com.foreflight.weather;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.foreflight.TestConfig;
-import com.foreflight.exception.WeatherNotFoundException;
-import com.foreflight.external.WeatherAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
 
-@Import(TestConfig.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WeatherControllerExceptionsIT {
 
     @Autowired private WebTestClient webTestClient;
-    @Autowired private WeatherAPI weatherAPI;
     private final String WEATHER_URI = "/v1/weather/";
 
     @Test
     void willThrowWeatherNotFoundException() {
         String ident = "INVALID_AIRPORT";
-
-        when(weatherAPI.findWeather(ident)).thenThrow(new WeatherNotFoundException("Weather data not found for identifier: " + ident));
 
         webTestClient.get()
                 .uri(WEATHER_URI + ident)
@@ -36,7 +27,6 @@ class WeatherControllerExceptionsIT {
                 .isNotFound() // Expecting 404 status due to the WeatherNotFoundException
                 .expectBody(JsonNode.class) // Expecting a JSON response
                 .value(response -> {
-                    verify(weatherAPI, times(1)).findWeather(ident);
                     assertThat(response.get("statusCode").asInt()).isEqualTo(404);
                     assertThat(response.get("message").asText()).isEqualTo("Weather data not found for identifier: " + ident);
                 });
--- src/test/java/com/foreflight/weather/WeatherControllerValidIT.java
+++ src/test/java/com/foreflight/weather/WeatherControllerValidIT.java
@@ -1,40 +1,26 @@
 package com.foreflight.weather;
 
-import com.foreflight.TestConfig;
-import com.foreflight.external.WeatherAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-@Import(TestConfig.class)
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WeatherControllerValidIT {
 
     @Autowired private WebTestClient webTestClient;
-    @Autowired private WeatherAPI weatherAPI;
     private final String WEATHER_URI = "/v1/weather/";
 
     @Test
     void canGetWeather() {
         String ident = "KFFC";
-
-        Weather mockedWeather = Weather.builder()
-                .ident("KFFC")
-                .build();
-
-        ResponseEntity<Weather> weatherResponse = ResponseEntity.ok(mockedWeather);
-        when(weatherAPI.findWeather(ident)).thenReturn(weatherResponse);
 
         List<WeatherDTO> weather = webTestClient.get()
                 .uri(WEATHER_URI + ident)
@@ -47,8 +33,6 @@ class WeatherControllerValidIT {
                 .returnResult()
                 .getResponseBody();
 
-        verify(weatherAPI).findWeather(ident);
-
         assertThat(weather).hasSize(1);
         WeatherDTO returnedWeather = weather.get(0);
         assertThat(returnedWeather.getIdent()).isEqualTo(ident);
@@ -57,20 +41,6 @@ class WeatherControllerValidIT {
     @Test
     void canGetWeatherForMultipleAirports() {
         String idents = "KFFC,KATL";
-
-        Weather mockedWeather1 = Weather.builder()
-                .ident("KFFC")
-                .build();
-
-        Weather mockedWeather2 = Weather.builder()
-                .ident("KATL")
-                .build();
-
-        ResponseEntity<Weather> weatherResponse1 = ResponseEntity.ok(mockedWeather1);
-        ResponseEntity<Weather> weatherResponse2 = ResponseEntity.ok(mockedWeather2);
-
-        when(weatherAPI.findWeather("KFFC")).thenReturn(weatherResponse1);
-        when(weatherAPI.findWeather("KATL")).thenReturn(weatherResponse2);
 
         List<WeatherDTO> weather = webTestClient.get()
                 .uri(WEATHER_URI + idents)
@@ -82,9 +52,6 @@ class WeatherControllerValidIT {
                 })
                 .returnResult()
                 .getResponseBody();
-
-        verify(weatherAPI).findWeather("KFFC");
-        verify(weatherAPI).findWeather("KATL");
 
         assertThat(weather).hasSize(2);
         WeatherDTO returnedWeather1 = weather.get(0);
